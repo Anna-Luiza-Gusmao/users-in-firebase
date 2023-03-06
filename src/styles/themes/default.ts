f74183f0b0a166ed7ab2d39d6f89ddca38665603
anna-luiza-haus <87679298+Anna-Luiza-Gusmao@users.noreply.github.com>
2023-03-06 10:06:04 -0300

    alter: colors theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,10 +1,10 @@
 export const defaultTheme = {
     'white': '#F2F2F2',
 
-    'orange-100': '#FFD393',
+    'orange-100': '#f5d8ae',
     'orange-150': '#ffebdd',
     'orange-200': '#FF974F',
-    'orange-400': '#f5883c',
+    'orange-400': '#f59654',
 
     'gray-100': '#8b8989',
     'gray-300': '#404040'
