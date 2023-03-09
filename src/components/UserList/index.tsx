1e82ba199ed22423c29090d3f499d8b90d6c9404
anna-luiza-haus <87679298+Anna-Luiza-Gusmao@users.noreply.github.com>
2023-03-09 14:35:18 -0300

    alter: dynamic pagination (with problem coming back)

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -18,26 +18,55 @@ const themePagination = createTheme({
         main: 'rgb(255,151,79, 0.2)'
       },
     },
-});
+})
 
 export function UserList() {
     const [userList, setUserList] = useState<DataUser[]>([])
     const allUsers = collection(firestore, "users")
+    const [page, setPage] = useState(1)
+    const [nextPage, setNextPage] = useState(page * 5)
+    const [currentPage, setCurrentPage] = useState(0)
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        if(value === page) {
+            return
+        }else{
+            if (value < page) {
+                setNextPage(nextPage - 5)
+            }else{
+                setNextPage(value * 5)
+            } 
 
-    async function loadUsers() {
-        let arrayUserList: DataUser[] = []
-        const querySnapshot = await getDocs(allUsers)
-        querySnapshot.forEach((doc) => {
-            arrayUserList.push({
-                name: doc.data().name,
-                email: doc.data().email,
-                typeUser: doc.data().typeUser
-            })
-        })
-        setUserList(arrayUserList)
+            setPage(value)
+    
+            if(value === 1) {
+                setCurrentPage(0)
+            }else{
+                setCurrentPage(page * 5)
+            }
+        }
     }
+    const totalPages = Math.ceil(userList.length / 5)
+    const newUserList = userList.slice(currentPage, nextPage)
+
+    console.log(currentPage, '-', nextPage)
 
     useEffect(() => {
+    }, [page])
+     
+    useEffect(() => {
+        async function loadUsers() {
+            let arrayUserList: DataUser[] = []
+            const querySnapshot = await getDocs(allUsers)
+            querySnapshot.forEach((doc) => {
+                arrayUserList.push({
+                    name: doc.data().name,
+                    email: doc.data().email,
+                    typeUser: doc.data().typeUser
+                })
+            })
+            setUserList(arrayUserList)
+        }
+
         loadUsers()
     }, [])
 
@@ -57,7 +86,7 @@ export function UserList() {
                                 </thead>
                                 <tbody>
                                     {
-                                        userList.map((user) => (
+                                        newUserList.map((user) => (
                                             <tr key={user.email}>
                                                 <td>{user.name}</td>
                                                 <td>{user.email}</td>
@@ -71,7 +100,14 @@ export function UserList() {
                     }
                 </TableUsers>
                 <ThemeProvider theme={themePagination}>
-                    <Pagination count={5} shape="rounded" color="primary"/>
+                    <Pagination 
+                        count={totalPages} 
+                        defaultPage={1}
+                        shape="rounded" 
+                        color="primary"
+                        page={page} 
+                        onChange={handleChange}
+                    />
                 </ThemeProvider>
             </TableContainer>
         </UserContainer>
